2678e6228bdc673fbc59d1b1807c8343d7e7fc4a
jiksearch <hamidk79@yahoo.com>
2024-01-14 19:58:09 +0330

    Add

---
+++ SQL H/SQL H/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SQL_H
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.BackColor = System.Drawing.Color.LavenderBlush;
+            this.groupBox6.Controls.Add(this.label3);
+            this.groupBox6.Controls.Add(this.label2);
+            this.groupBox6.Controls.Add(this.label1);
+            this.groupBox6.Controls.Add(this.button1);
+            this.groupBox6.Controls.Add(this.textBoxServer);
+            this.groupBox6.Controls.Add(this.textBoxUser);
+            this.groupBox6.Controls.Add(this.textBoxPass);
+            this.groupBox6.Controls.Add(this.buttonInfo);
+            this.groupBox6.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox6.ForeColor = System.Drawing.Color.Black;
+            this.groupBox6.Location = new System.Drawing.Point(12, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(530, 153);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "DataBase";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 14);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 14);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "User:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 14);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "IP:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Get info by this user access";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(93, 27);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(146, 22);
+            this.textBoxServer.TabIndex = 25;
+            this.textBoxServer.Text = ".";
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(108, 61);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(131, 22);
+            this.textBoxUser.TabIndex = 24;
+            this.textBoxUser.Text = "sa";
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(139, 100);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPass.TabIndex = 23;
+            this.textBoxPass.Text = "123";
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Location = new System.Drawing.Point(304, 28);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(157, 23);
+            this.buttonInfo.TabIndex = 9;
+            this.buttonInfo.Text = "Get all DB info";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(530, 242);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(12, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "2024@KarimiH@gmail.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox6);
+            this.Name = "Form1";
+            this.Text = "SQL info";
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button buttonInfo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SQL H/SQL H/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SQL_H
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connStrBldr = new System.Data.SqlClient.SqlConnectionStringBuilder();
+                connStrBldr.DataSource = textBoxServer.Text.Trim();
+
+                connStrBldr.IntegratedSecurity = false;
+                connStrBldr.UserID = textBoxUser.Text.Trim();
+                connStrBldr.Password = textBoxPass.Text.Trim();
+
+                using (SqlConnection sqlCon = new SqlConnection(connStrBldr.ToString()))
+                {
+                    sqlCon.Open();
+                    //SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT  sys.databases.name,  CONVERT(VARCHAR,SUM(size)*8/1024)+' MB' AS [TotalDiskSpace]  FROM sys.databases JOIN sys.master_files ON  sys.databases.database_id=sys.master_files.database_id GROUP BY sys.databases.name  ORDER BY sys.databases.name ", sqlCon);
+                    SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT   sys.databases.[name], IIF([type]= 0 , 'Main','log') AS Type,  CONVERT(VARCHAR,(size*8/1024))+'MB' AS Size FROM sys.databases JOIN sys.master_files ON  sys.databases.database_id=sys.master_files.database_id ORDER BY sys.databases.name ", sqlCon);
+                    DataTable dtbl = new DataTable();
+                    sqlDa.Fill(dtbl);
+                    dataGridView1.DataSource = dtbl;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connStrBldr = new System.Data.SqlClient.SqlConnectionStringBuilder();
+                connStrBldr.DataSource = textBoxServer.Text.Trim();
+
+                connStrBldr.IntegratedSecurity = false;
+                connStrBldr.UserID = textBoxUser.Text.Trim();
+                connStrBldr.Password = textBoxPass.Text.Trim();
+
+                using (SqlConnection sqlCon = new SqlConnection(connStrBldr.ToString()))
+                {
+                    sqlCon.Open();
+                    SqlDataAdapter sqlDa = new SqlDataAdapter("select [name], type,  CONVERT(VARCHAR,(size*8/1024))+'MB' AS Size   from sys.database_files", sqlCon);
+                    DataTable dtbl = new DataTable();
+                    sqlDa.Fill(dtbl);
+                    dataGridView1.DataSource = dtbl;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
